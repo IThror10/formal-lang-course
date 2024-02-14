ede8c9bc5aae72448661651208825859913c7433
Skom Erik <erik.skom@yandex.ru>
2024-02-14 21:39:39 +0300

    [HW-#1.2] Style fixes and python version decrement

--- project/hw1/task.py
+++ project/hw1/task.py
@@ -6,9 +6,12 @@ from networkx.drawing.nx_pydot import write_dot
 def getGraphInfoByName(graphName: str):
     graph_csv = cfpq_data.download(graphName)
     graph = cfpq_data.graph_from_csv(graph_csv)
-    return  graph.number_of_nodes(),\
-            graph.number_of_edges(),\
-            set(map(lambda x: x[2]['label'], graph.edges(data=True)))
+    return (
+        graph.number_of_nodes(),
+        graph.number_of_edges(),
+        set(map(lambda x: x[2]["label"], graph.edges(data=True))),
+    )
+
 
 def createBiSycleGraph(cSize1: int, cSize2: int, labels: set[str], path: str):
     graph = labeled_two_cycles_graph(n=cSize1, m=cSize2, labels=labels)
